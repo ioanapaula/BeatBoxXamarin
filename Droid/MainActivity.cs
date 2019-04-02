41cff2275c9d81105ea546ccc44f2a15e193b69c
Ioana-Paula Stroe <stroe.ioanapaula@gmail.com>
2019-04-02 12:37:00 +0300

    Initial commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace BeatBoxXamarin.Droid
+{
+    [Activity(Label = "BeatBoxXamarin", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+    }
+}
